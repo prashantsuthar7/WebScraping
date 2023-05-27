cc37597191d1c5e5911a9157aa6c74d7d757bd21
prash <prashantsuthar7@gmail.com>
2023-05-27 11:48:13 +0530

    webScraping_architectdirectory is website for upwork.

--- accesExcel.py
+++ accesExcel.py
@@ -1,6 +1,6 @@
 from openpyxl import load_workbook
 
-wb = load_workbook("C:/Users/prash/Desktop/ZIP/ZipFolder/Zip_Code_missing.xlsx")  # Work Book
+wb = load_workbook("P:/Destop/ZIP/ZipFolder/Zip_Code_missing.xlsx")  # Work Book
 ws = wb.get_sheet_by_name('New')  # Work Sheet
 column = ws['B']  # Column
 column_list = [column[x].value for x in range(len(column))]
